ca80d7b6661fb6acdcb9c86dc9fb8a7d90baba2d
Harsh Gyanchandani <harsh.gyanchandani@gmail.com>
2020-10-06 16:25:47 +0530

    added registration screens

---
+++ src/controllers/RegisterController.java
@@ -0,0 +1,41 @@
+package controllers;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class RegisterController {
+
+    @FXML
+    public JFXButton registerBTN;
+    @FXML
+    public Text loginTxt;
+    public JFXButton loginBTN;
+    @FXML
+    private TextField nameTF,emailTF,passTF,cnfpassTF;
+
+    public void onRegisterClick(ActionEvent actionEvent) {
+    }
+
+    public void loginButtonClicked(ActionEvent actionEvent) throws IOException {
+        // Scene to be displayed
+        Parent loginScreenParent = FXMLLoader.load(getClass().getResource("../sample/login.fxml"));
+        Scene loginScreenScene = new Scene(loginScreenParent);
+
+        // Getting the current stage window
+        Stage window = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+
+        // Setting the new scene in the window
+        window.setScene(loginScreenScene);
+        window.show();
+    }
+}
---
+++ src/sample/LoginController.java
@@ -0,0 +1,39 @@
+package sample;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.Node;
+import javafx.scene.control.PasswordField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class LoginController {
+
+    public JFXButton signupBTN;
+    @FXML
+    private PasswordField passwordTF;
+
+    public void onSignInClicked(ActionEvent actionEvent) {
+        System.out.println("Login clicked!");
+        passwordTF.setText("Hello");
+    }
+
+    public void onSignupClicked(ActionEvent actionEvent) throws IOException {
+
+        // Scene to be displayed
+        Parent registerScreenParent = FXMLLoader.load(getClass().getResource("../resources/register.fxml"));
+        Scene registerScreenScene = new Scene(registerScreenParent);
+
+        // Getting the current stage window
+        Stage window = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+
+        // Setting the new scene in the window
+        window.setScene(registerScreenScene);
+        window.show();
+    }
+}
