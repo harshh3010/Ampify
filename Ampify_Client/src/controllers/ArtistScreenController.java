cbce577e89136c95f1e33545ecbae07716b94ad0
Harsh Gyanchandani <harsh.gyanchandani@gmail.com>
2020-10-20 21:54:58 +0530

    added artist screen

--- Ampify_Client/src/controllers/ArtistScreenController.java
+++ Ampify_Client/src/controllers/ArtistScreenController.java
@@ -5,6 +5,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import model.Artist;
 
@@ -14,9 +16,14 @@ import java.util.ResourceBundle;
 
 public class ArtistScreenController implements Initializable {
 
+
     private Artist artist;
     private Pane homePageDisplayPane;
 
+    @FXML
+    public ImageView imageView;
+    @FXML
+    public Label ratingLabel;
     @FXML
     public Label nameLabel;
 
@@ -25,23 +32,25 @@ public class ArtistScreenController implements Initializable {
 
     }
 
-    public void saveArtistDetails(Artist artist, Pane homePageDisplayPane){
+    public void saveArtistDetails(Artist artist, Pane homePageDisplayPane) {
         this.artist = artist;
         this.homePageDisplayPane = homePageDisplayPane;
+
         nameLabel.setText(artist.getArtistName());
+        ratingLabel.setText("⭐" + String.valueOf(artist.getArtistRating()));
+        imageView.setImage(new Image(artist.getArtistImageURL()));
+        imageView.setPreserveRatio(false);
+//        imageView.setStyle("-fx-background-image: url(\"" + artist.getArtistImageURL() + "\"); -fx-background-size: cover;");
     }
 
 
     public void onBackClicked(ActionEvent actionEvent) {
-
         try {
-            Pane newPane =  FXMLLoader.load(getClass().getResource("/resources/fxml/homeContentsPane.fxml"));
+            Pane newPane = FXMLLoader.load(getClass().getResource("/resources/fxml/homeContentsPane.fxml"));
             homePageDisplayPane.getChildren().remove(0);
             homePageDisplayPane.getChildren().add(newPane);
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-
     }
 }
