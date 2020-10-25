1aaf7c2d3ed031a8eb4a982cdd56698dfa541626
Harsh Gyanchandani <harsh.gyanchandani@gmail.com>
2020-10-25 15:10:54 +0530

    commit before pull

--- Ampify_Client/src/controllers/ArtistCellController.java
+++ Ampify_Client/src/controllers/ArtistCellController.java
@@ -6,10 +6,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.control.ContentDisplay;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
+import javafx.scene.image.Image;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.*;
 import model.Artist;
 
 import java.io.IOException;
@@ -48,18 +48,33 @@ public class ArtistCellController extends ListCell<Artist> {
             setText(null);
             setContentDisplay(ContentDisplay.TEXT_ONLY);
         } else {
+
+            // Displaying the name of artist
             nameLabel.setText(artist.getArtistName());
-            mainCard.setOnMouseClicked(new EventHandler<MouseEvent>() {
+
+            // Creating a background using image of artist
+            BackgroundImage backgroundImage = new BackgroundImage(
+                    new Image(artist.getArtistImageURL()),
+                    BackgroundRepeat.REPEAT,
+                    BackgroundRepeat.NO_REPEAT,
+                    BackgroundPosition.CENTER,
+                    new BackgroundSize(100, 100, true, true, false, true));
+
+            // Setting the background in main artist card
+            mainCard.setBackground(new Background(backgroundImage));
+
+            // Adding mouse click listener to the artist card
+            mainCard.setOnMouseClicked(new EventHandler<>() {
                 @Override
                 public void handle(MouseEvent mouseEvent) {
-                    if (mouseEvent.getButton().equals(MouseButton.PRIMARY)) {
-                        if (mouseEvent.getClickCount() == 2) {
+                    if (mouseEvent.getButton().equals(MouseButton.PRIMARY)) {   // Ensuring LMB is clicked
+                        if (mouseEvent.getClickCount() == 2) {  // Ensuring its a double click
                             try {
                                 // Move to artist screen on double click
-                                FXMLLoader loader =  new FXMLLoader(getClass().getResource("/resources/fxml/artistScreen.fxml"));
+                                FXMLLoader loader = new FXMLLoader(getClass().getResource("/resources/fxml/artistScreen.fxml"));
                                 Pane newLoadedPane = loader.load();
                                 ArtistScreenController artistScreenController = loader.getController();
-                                artistScreenController.saveArtistDetails(artist,displayPane);
+                                artistScreenController.saveArtistDetails(artist, displayPane);
                                 displayPane.getChildren().remove(0);
                                 displayPane.getChildren().add(newLoadedPane);
 
